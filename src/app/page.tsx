bf994ebe8c4617640662ba4cd0b7289c59a93420
2lay <mail@2lay.net>
2024-11-26 21:04:43 +0100

    feat: elysia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default async function Home() {
+  const protocol = process.env.NODE_ENV === 'development' ? 'http:' : 'https:';
+  const host = process.env.NEXT_PUBLIC_VERCEL_URL || 'localhost:1337';
+  const baseUrl = `${protocol}//${host}`;
+
+  const response = await fetch(`${baseUrl}/api`);
+  const data = await response.text();
+
   return (
     <div>
-      <h1>hi</h1>
+      <h1>Message from Elysia:</h1>
+      <p>{data}</p>
     </div>
   );
 }
