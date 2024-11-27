2ae95c29b2c6a6a87d5c9bd27219deafcb5f14cc
2lay <mail@2lay.net>
2024-11-27 08:26:34 +0100

    feat: google font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 export default async function Home() {
-  const protocol = process.env.NODE_ENV === 'development' ? 'http:' : 'https:';
-  const host = process.env.NEXT_PUBLIC_VERCEL_URL || 'localhost:1337';
-  const baseUrl = `${protocol}//${host}`;
+    const protocol = process.env.NODE_ENV === 'development' ? 'http:' : 'https:';
+    const host = process.env.NEXT_PUBLIC_VERCEL_URL || 'localhost:1337';
+    const baseUrl = `${protocol}//${host}`;
 
-  const response = await fetch(`${baseUrl}/api`);
-  const data = await response.text();
+    const response = await fetch(`${baseUrl}/api`);
+    const data = await response.text();
 
-  return (
-    <div>
-      <h1>Message from Elysia:</h1>
-      <p>{data}</p>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Message from Elysia:</h1>
+            <p>{data}</p>
+        </div>
+    );
 }
